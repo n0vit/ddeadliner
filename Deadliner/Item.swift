69a0962b9fde8b4947e7df0388f5c50c036a4a31
Nickolas <kolzazanovikov@gmail.com>
2024-05-12 11:53:40 +0700

    Initial Commit

---
+++ Deadliner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Deadliner
+//
+//  Created by NIKOLAI on 12.05.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
